cabfa80f1a683241e66ab3fccd6b1ccae353f94f
Cameron Braverman <bravermancj@gmail.com>
2018-04-08 20:49:26 -0400

    added views and picture rendering

--- index.js
+++ index.js
@@ -3,22 +3,44 @@ const path = require("path")
 const app = express();
 const bodyParser = require('body-parser');
 const multer = require('multer');
-var upload = multer({ dest: 'uploads/' })
+var fs = require('fs');
+
+app.use(express.static(path.join(__dirname, 'uploads')));
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')) );
+
+var storage = multer.diskStorage({
+	destination: './uploads/',
+	filename: function (req, file, cb) {
+		cb(null, file.originalname)
+	}
+  });
+
+var upload = multer({ storage: storage })
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 
 app.use(express.json());
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit:50000}));
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 8080
 
 app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname + '/index.html'));
+	fs.readdir( "./uploads", function( err, files ) {
+		let pics = []
+		files.forEach(function(file, index){
+			pics.push(file);
+		});
+		res.render('main', {images: pics})
+	});
 });
 
 app.post("/cool", function (req, res) {
 	console.log(req.body.cool);
-	
 	res.send(req.body.nothing);
 });
 
@@ -26,9 +48,8 @@ app.post("/upload", upload.single('image'), (req, res) => {
 	let image = req.file;
 	let body = req.body;
 	console.log("Image body \(body)");
-	console.log(image);
-	res.send('You gucci')
-	res.sendStatus(200);
+	res.status(200).send('You gucci');
+	res.end();
 });
 
 
